3c8c5010efca02bb53c9f6d22cbe1505fabf56d2
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-02-01 18:46:46 -0500

    Styling for explore component

--- Frontend/src/components/Courses/ExploreCourses.jsx
+++ Frontend/src/components/Courses/ExploreCourses.jsx
@@ -1,32 +1,34 @@
 import CourseCards from "./CourseCards";
 import { useState } from "react";
+import '../../styles/exploreCourses.css'
 function ExploreCourses(){
   const [category, setCategory] = useState(null);
+  const [subcategory, setSubcategory] = useState("");
   return(
     <div className="explore-page-container">
       <div className="explore-page-banner">
         <div className="explore-page-banner-text">
-          <h3></h3>
-          <p></p>
+          <h1>Explore our courses<i className="fa-solid fa-lightbulb"> </i></h1>
+          <p>Browse through a variety of courses designed to help you learn and grow.</p>
         </div>
-        <button>View all courses</button>
+        <button className="explore-page-view-all">View all courses</button>
       </div>
       <div className="explore-page-interactions">
         <div className="explore-page-buttons">
-          <button>Science</button>
-          <button>Math</button>
-          <button>English</button>
-          <button>CS</button>
-          <button>Engineering</button>
+          <button className="explore-page-button-active">Science</button>
+          <button className="explore-page-button">Math</button>
+          <button className="explore-page-button">English</button>
+          <button className="explore-page-button">CS</button>
+          <button className="explore-page-button">Engineering</button>
         </div>
         <div className="explore-page-nav">
           <div className="explore-page-sub-category">
-            <div>All</div>
+            <div>All{subcategory===""?<span className="selector-line"></span>:''}</div>
             <div>Chemistry</div>
             <div>Physics</div>
-            <div>Biology</div>;
+            <div>Biology</div>
           </div>
-          <div className="explore-page-search"><i className="fa-solid fa-magnifying-glass"></i><input></input></div>
+          <div className="explore-page-search"><i className="fa-solid fa-magnifying-glass"></i><input placeholder="Search course"></input></div>
         </div>
       </div>
       <div>
